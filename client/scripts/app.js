e4e7ce5cda6d4d8b2322889eea4059af86ea523a
staybailey <staybailey@gmail.com>
2015-12-07 17:35:22 -0800

    Got fetch working seemingly

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -7,12 +7,16 @@ app.init = function(){};
 app.server = 'https://api.parse.com/1/classes/chatterbox';
 
 app.fetch = function(){
+  var that = this;
   $.ajax({
     url: 'https://api.parse.com/1/classes/chatterbox',
     type: 'GET',
     success: function (data) {
       console.log('fetch success!');
-      console.log(data);
+      var results = data.results;
+      for(var i = 0; i < results.length; i++) {
+        that.addMessage(results[i]);
+      }
     },
     error: function (data) {
     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -39,25 +43,45 @@ app.send = function (message) {
 };
 
 app.clearMessages = function(){
-  $('#chats').empty();
+  $('.chats').empty();
 };
 
 app.addMessage = function(message){
-  $('#chats').append('<p>' + message.username + ": " + message.text + '</p>')
+  message = this.sanitizeMessage(message);
+  $('.chats').append('<p id="' + message.username + '">' + message.username + ": " + message.text + '</p>');
 };
 
 app.addRoom = function(room) {
-  $('#roomSelect').append('<option value="' + room + '">' + room + '</option>');
+  $('.roomSelect').append('<option value="' + room + '">' + room + '</option>');
 };
 
+app.addFriend = function(friend) {
+  $('[id=' + friend + ']').css("font-weight", "Bold");
+};
+
+app.sanitizeMessage = function(message) {
+    _.each(message, function (item, key) {
+      if (typeof item === 'string') {
+        message[key] = item.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/"/g, '&quot;')
+      } else {
+        message[key] = 'Invalid Input';
+      }
+    });
+    return message;
+};
+
+$('#Bailey').on('click', function(){
+    app.addFriend('Bailey');
+  })
+
+
 $(document).ready(function() {
-
-
-
-
-
-  $('body button').on('click', function () {
+  $('.submit').on('click', function () {
     var text = $('textarea').val();
-    send(text);
+    app.send(text);
+  });
+
+  $('.clear').on('click', function () {
+    app.clearMessages();
   });
 });
