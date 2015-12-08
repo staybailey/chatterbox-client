ec270012d7cd303559fdc512355f41ff98c410f8
staybailey <staybailey@gmail.com>
2015-12-07 16:22:59 -0800

    Initial draft with spec to add friend working

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,63 @@
 // YOUR CODE HERE:
+  
+var app = {};
+
+app.init = function(){};
+
+app.server = 'https://api.parse.com/1/classes/chatterbox';
+
+app.fetch = function(){
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'GET',
+    success: function (data) {
+      console.log('fetch success!');
+      console.log(data);
+    },
+    error: function (data) {
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to retrieve');
+    }
+  });
+};
+
+app.send = function (message) {  
+  $.ajax({
+  // This is the url you should use to communicate with the parse API server.
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+};
+
+app.clearMessages = function(){
+  $('#chats').empty();
+};
+
+app.addMessage = function(message){
+  $('#chats').append('<p>' + message.username + ": " + message.text + '</p>')
+};
+
+app.addRoom = function(room) {
+  $('#roomSelect').append('<option value="' + room + '">' + room + '</option>');
+};
+
+$(document).ready(function() {
+
+
+
+
+
+  $('body button').on('click', function () {
+    var text = $('textarea').val();
+    send(text);
+  });
+});
